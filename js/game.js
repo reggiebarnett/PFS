67d13c669b6d516dcce43277c9f334bb144eb0a0
reggiebarnett <reggiedbarnett@gmail.com>
2016-07-14 11:40:39 -0400

    updated controls even more

--- js/game.js
+++ js/game.js
@@ -3,12 +3,16 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 var diver = new player("red");
-var INIT_HEIGHT = 10000; //initial height for testing
+var INIT_HEIGHT = 5000; //initial height for testing
+var LOW_LIMIT = 2000;
+var DIVER_START_X = 90;
+var DIVER_START_Y = 100;
 var ht = new heightTracker();
 var speedUp;
 var slowDown;
 var glideLeft;
 var glideRight;
+var DIVE_ACCEL = 3;
 
 //Controls
 document.addEventListener("keydown",keyDownHandler, false);
@@ -16,15 +20,20 @@ document.addEventListener("keyup",keyUpHandler,false);
 
 function keyDownHandler(e){
 	//freefall controls
-		//TODO: disable once parachute deploys
+		//TODO: disable once parachute deploys, WIP
 	if(e.keyCode == 38 || e.keyCode == 87){ //up arrow or w
-		slowDown = true;
+		if(ht.height > LOW_LIMIT){
+			slowDown = true;
+		}
 	} 
 	else if(e.keyCode == 40 || e.keyCode == 83){ //down arrow or s
-		speedUp = true;
+		if(ht.height > LOW_LIMIT){
+			speedUp = true;
+		}
+
 	} 
 	//parachute controls
-		//TODO: enable once parachute deploys
+		//TODO: enable once parachute deploys, WIP
 	else if(e.keyCode == 37 || e.keyCode == 65){ //left arrow or a
 		glideLeft = true;
 	} 
@@ -35,15 +44,24 @@ function keyDownHandler(e){
 
 function keyUpHandler(e){
 	//freefall controls
-		//TODO: disable once parachute deploys
+		//TODO: disable once parachute deploys, WIP
 	if(e.keyCode == 38 || e.keyCode == 87){ //up arrow or w
 		slowDown = false;
+		//Prevent resetting after passing LOW_LIMIT
+		if(ht.height > LOW_LIMIT){
+			diving_actions("return");
+		}
 	} 
 	else if(e.keyCode == 40 || e.keyCode == 83){ //down arrow or s
 		speedUp = false;
+		//Prevent resetting after passing LOW_LIMIT
+		if(ht.height > LOW_LIMIT){
+			diving_actions("return");
+		}
+
 	} 
 	//parachute controls
-		//TODO: enable once parachute deploys
+		//TODO: enable once parachute deploys, WIP
 	else if(e.keyCode == 37 || e.keyCode == 65){ //left arrow or a
 		glideLeft = false;
 	} 
@@ -52,6 +70,26 @@ function keyUpHandler(e){
 	} 
 }
 
+//calculating movements for slowing down and speeding up
+function diving_actions(action) {
+	if(action === "slow"){
+		//X pos
+		diver.x = 150;
+		//Y pos
+		diver.y = 70;
+	}
+	else if(action === "speed"){
+		//X pos
+		diver.x = 120;
+		//Y pos
+		diver.y = 160;
+	}
+	else if(action === "return"){
+		//return to original position
+		diver.x = DIVER_START_X;
+		diver.y = DIVER_START_Y;
+	}
+}
 
 
 
@@ -61,21 +99,35 @@ function player(color){
 	//TODO: replace width,height, ctx fill stuff with diver image
 	this.width = 30;
 	this.height = 30;
-	this.x = 90; 
-	this.y = 100;
+	this.x = DIVER_START_X; 
+	this.y = DIVER_START_Y;
+	ctx.fillStyle = color;
+	ctx.fillRect(this.x,this.y, this.width,this.height);
 	this.update = function(){
-		//can only speed up slow down above 2000 ft
-		if(speedUp && ht.height > 2000){
-			this.y += 2;
+		//can only speed up slow down above LOW_LIMIT
+		if(speedUp){
+			//temp fix until I figure out better way to prevent holding down buttons
+			if(ht.height <= LOW_LIMIT){
+				diving_actions("return");
+			}
+			if(ht.height > LOW_LIMIT){
+				diving_actions("speed");
+			}
 		}
-		else if(slowDown && ht.height > 2000){
-			this.y -= 2;
+		else if(slowDown){
+			//temp fix until I figure out better way to prevent holding down buttons
+			if(ht.height <= LOW_LIMIT){
+				diving_actions("return");
+			}
+			if(ht.height > LOW_LIMIT){
+				diving_actions("slow");
+			}
 		}
-		//can only glide at or below 2000 ft
-		else if(glideLeft && ht.height <= 2000){
+		//can only glide at or below LOW_LIMIT
+		else if(glideLeft && ht.height <= LOW_LIMIT){
 			this.x -= 2;
 		}
-		else if(glideRight && ht.height <= 2000){
+		else if(glideRight && ht.height <= LOW_LIMIT){
 			this.x += 2;
 		}
 		ctx.fillStyle = color;
